6640d9e9ab6b6d68a569682f18fa976239acd750
jangjh123 <gn0001231@gmail.com>
2023-11-11 23:23:49 +0900

    feat: CoroutineDispatcher DI 구현
    Related To: #4

---
+++ client/presentation/src/main/java/com/everyone/movemove_android/di/CoroutineDispatcherModule.kt
@@ -0,0 +1,20 @@
+package com.everyone.movemove_android.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutineDispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
---
+++ client/presentation/src/main/java/com/everyone/movemove_android/di/CoroutineDispatcherQualifiers.kt
@@ -0,0 +1,11 @@
+package com.everyone.movemove_android.di
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class IoDispatcher
