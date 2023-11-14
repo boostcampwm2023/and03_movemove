5de4e046f714a4fd2fbd4ac30121f8401a35d308
msjang4 <msjang7115@gmail.com>
2023-11-14 15:40:47 +0900

    chore : 필수 모듈 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+import { ActionModule } from './action/action.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, VideoModule, ActionModule],
   controllers: [AppController],
   providers: [AppService],
 })
