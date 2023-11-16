d41bd237a4d4b621e1d7f3bbfb490e2ee15bb297
upsk1 <upsk1@naver.com>
2023-11-16 21:33:25 +0900

    feat: 카카오 로그인 버튼 기능 구현

--- client/presentation/src/main/java/com/everyone/movemove_android/ui/login/LoginScreen.kt
+++ client/presentation/src/main/java/com/everyone/movemove_android/ui/login/LoginScreen.kt
@@ -1,5 +1,7 @@
 package com.everyone.movemove_android.ui.login
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -18,14 +20,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.everyone.movemove_android.R.drawable
 import com.everyone.movemove_android.ui.theme.KakaoYellow
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
 @Composable
 fun LoginScreen() {
-    Log.d("실행", "됐나요?")
+    val context = LocalContext.current
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -39,7 +46,9 @@ fun LoginScreen() {
                 .padding(horizontal = 10.dp)
         ) {
             Button(
-                onClick = {},
+                onClick = {
+                    handleKakaoLogin(context)
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(48.dp),
@@ -57,10 +66,46 @@ fun LoginScreen() {
                     )
                     Text(
                         text = "카카오 로그인",
-                        modifier = Modifier.align(alignment = Alignment.Center)
+                        modifier = Modifier.align(alignment = Alignment.Center),
                     )
                 }
             }
         }
+    }
+}
+
+fun handleKakaoLogin(context: Context) {
+
+    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e(TAG, "카카오계정으로 로그인 실패", error)
+        } else if (token != null) {
+            Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+            UserApiClient.instance.me { user, _ ->
+                Log.i(TAG, "계정 uid ${user!!.id}")
+            }
+        }
+    }
+
+// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+        UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+            if (error != null) {
+                Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    return@loginWithKakaoTalk
+                }
+
+                // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            } else if (token != null) {
+                Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+            }
+        }
+    } else {
+        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
     }
 }
