6fc79e08e2d0ada6208d87dc57abd2080bb17ecb
msjang4 <msjang7115@gmail.com>
2023-11-18 14:03:30 +0900

    chore : UserDTO에 Swagger 설정

--- server/src/user/dto/user.dto.ts
+++ server/src/user/dto/user.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserDto {
+  @ApiProperty({ description: 'UUID', format: 'uuid' })
   uuid?: string;
+  @ApiProperty({ description: '유저 프로필 사진', format: 'binary' })
   profileImage: string;
+  @ApiProperty({ description: '유저 닉네임', example: '페이커' })
   nickname: string;
+  @ApiProperty({
+    description: '한줄 소개',
+    example: '모든 길은 저를 통할 것입니다',
+  })
   statusMessage: string;
 }
