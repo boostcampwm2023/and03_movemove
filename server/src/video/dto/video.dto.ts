f846f932ab3bb92fb4e5da3611f79eecb7f22d98
5tarry <hacherry05@naver.com>
2023-11-20 21:44:36 +0900

    refactor : video dto 수정

--- server/src/video/dto/video.dto.ts
+++ server/src/video/dto/video.dto.ts
@@ -1,15 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class VideoDto {
+  /**
+   * 비디오 제목
+   */
   @IsNotEmpty()
   title: string;
 
+  /**
+   * 비디오 내용
+   */
   @IsNotEmpty()
   content: string;
 
-  video: string;
+  @ApiProperty({
+    description: '비디오 파일',
+    type: 'string',
+    format: 'binary',
+  })
+  video: Express.Multer.File;
 
+  /**
+   * 비디오 카테고리
+   * @example '챌린지'
+   */
   category: string;
 
-  thumbnail: string;
+  @ApiProperty({
+    description: '비디오 썸네일 이미지',
+    type: 'string',
+    format: 'binary',
+  })
+  thumbnail: Express.Multer.File;
 }
