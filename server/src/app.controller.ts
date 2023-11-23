b8d2a9e1ddf578de4104667e632179f7f487646b
msjang4 <msjang7115@gmail.com>
2023-11-23 11:21:56 +0900

    test : 테스트 Api 삭제

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,10 +1,5 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { requestEncoding } from './ncpAPI/requestEncoding';
-import { putObject } from './ncpAPI/putObject';
-import { getObject } from './ncpAPI/getObject';
-import { deleteObject } from './ncpAPI/deleteObject';
-import { AuthGuard } from './auth/auth.guard';
 
 @Controller()
 export class AppController {
