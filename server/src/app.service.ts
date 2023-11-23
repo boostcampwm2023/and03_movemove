667e0f49afd8ca294a319e805b646c6b3ded7b8f
5tarry <hacherry05@naver.com>
2023-11-23 18:02:42 +0900

    feat : 광고 이미지 반환

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,14 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { xml2js } from 'xml-js';
 import * as _ from 'lodash';
 import { listObjects } from './ncpAPI/listObjects';
+import { getObject } from './ncpAPI/getObject';
 
 @Injectable()
 export class AppService {
   async getAds() {
     const xmlData = await listObjects(process.env.ADVERTISEMENT_BUCKET);
     const jsonData: any = xml2js(xmlData, { compact: true });
+
     const adList = _.map(jsonData.ListBucketResult.Contents, 'Key._text');
-    console.log(adList);
-    return `get ads`;
+    const adImages = await Promise.all(
+      adList.map(async (ad: string) => {
+        const adImage = await getObject(process.env.ADVERTISEMENT_BUCKET, ad);
+        return adImage;
+      }),
+    );
+
+    return { adImages };
   }
 }
