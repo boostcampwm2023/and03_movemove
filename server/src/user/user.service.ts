07cb9c4eb09b41a6fe30dfaad7e62400db6a1ce1
msjang4 <msjang7115@gmail.com>
2023-11-23 16:28:10 +0900

    feat : 프로필 정보 조회 구현

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserNotFoundException } from 'src/exceptions/user-not-found.exception';
+import { getObject } from 'src/ncpAPI/getObject';
 import { ProfileDto } from './dto/profile.dto';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  getProfile(userId: string) {
-    return `get profile ${userId}`;
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+
+  async getProfile(userId: string) {
+    const user = await this.UserModel.findOne({ uuid: userId });
+
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+    const { uuid, profileImageExtension, nickname, statusMessage } = user;
+    const profileImage = profileImageExtension
+      ? await getObject(
+          process.env.PROFILE_BUCKET,
+          `${uuid}.${profileImageExtension}`,
+        )
+      : null;
+    return new ProfileDto({
+      nickname,
+      statusMessage,
+      ...(profileImage && { profileImage }),
+    });
   }
 
   patchProfile(
