29ba1810ae4c70dffbf66fb30c579374e3e85b6d
5tarry <hacherry05@naver.com>
2023-11-23 16:54:17 +0900

    feat : NCP ListObjects API 요청을 보내 광고 리스트 가져오기

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { xml2js } from 'xml-js';
+import * as _ from 'lodash';
+import { listObjects } from './ncpAPI/listObjects';
 
 @Injectable()
 export class AppService {
-  getAds() {
+  async getAds() {
+    const xmlData = await listObjects(process.env.ADVERTISEMENT_BUCKET);
+    const jsonData: any = xml2js(xmlData, { compact: true });
+    const adList = _.map(jsonData.ListBucketResult.Contents, 'Key._text');
+    console.log(adList);
     return `get ads`;
   }
 }
