51797ddad22de33abf81413ba8de902118eb180e
5tarry <hacherry05@naver.com>
2023-11-29 11:57:48 +0900

    feat : Object Storage의 Presigned URL 생성

---
+++ server/src/ncpAPI/presignedURL.ts
@@ -0,0 +1,32 @@
+import { HttpRequest } from '@smithy/protocol-http';
+import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
+import { parseUrl } from '@smithy/url-parser';
+import { formatUrl } from '@aws-sdk/util-format-url';
+import { Hash } from '@smithy/hash-node';
+
+export const createPresignedUrlWithoutClient = async ({
+  bucketName,
+  objectName,
+  method,
+}) => {
+  const region = 'kr-standard';
+  const endPoint = 'https://kr.object.ncloudstorage.com';
+  const canonicalURI = `/${bucketName}/${objectName}`;
+  const apiUrl = `${endPoint}${canonicalURI}`;
+  const url = parseUrl(apiUrl);
+
+  const presigner = new S3RequestPresigner({
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY,
+      secretAccessKey: process.env.SECRET_KEY,
+    },
+    region,
+    sha256: Hash.bind(null, 'sha256'),
+  });
+
+  const signedUrlObject = await presigner.presign(
+    new HttpRequest({ ...url, method }),
+    { expiresIn: 100 },
+  );
+  return formatUrl(signedUrlObject);
+};
