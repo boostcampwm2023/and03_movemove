9e481cac1d35f42eb5c2b45c269575ad58ed9252
jangjh123 <gn0001231@gmail.com>
2023-11-29 13:41:01 +0900

    chore: ImageCropperActivity 추가

---
+++ client/presentation/src/main/java/com/everyone/movemove_android/ui/image_cropper/ImageCropperActivity.kt
@@ -0,0 +1,11 @@
+package com.everyone.movemove_android.ui.image_cropper
+
+import androidx.compose.runtime.Composable
+import com.everyone.movemove_android.base.BaseActivity
+
+class ImageCropperActivity : BaseActivity() {
+    @Composable
+    override fun InitComposeUi() {
+        ImageCropperScreen()
+    }
+}
