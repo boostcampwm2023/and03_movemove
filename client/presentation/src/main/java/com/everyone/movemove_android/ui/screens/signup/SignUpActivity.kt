9d3cc4bb6e1864a501f6d67bc49b391d4829781b
upsk1 <upsk1@naver.com>
2023-11-30 14:50:05 +0900

    feat: signup activity 생성

---
+++ client/presentation/src/main/java/com/everyone/movemove_android/ui/screens/signup/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.everyone.movemove_android.ui.screens.signup
+
+import androidx.compose.runtime.Composable
+import com.everyone.movemove_android.base.BaseActivity
+
+class SignUpActivity : BaseActivity() {
+    @Composable
+    override fun InitComposeUi() {
+        SignUpScreen()
+    }
+}
