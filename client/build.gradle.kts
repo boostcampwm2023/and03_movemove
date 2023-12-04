aa38af6ba4c99c96ed09383263ab5d359f633395
JunJangE <wnswkd486@gmail.com>
2023-12-04 11:31:19 +0900

    chore: google.services 추가

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.parcelize) apply false
+    alias(libs.plugins.google.services) apply false
 }
--- client/presentation/build.gradle.kts
+++ client/presentation/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.lang)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    id("com.google.gms.google-services")
 }
 
 android {
