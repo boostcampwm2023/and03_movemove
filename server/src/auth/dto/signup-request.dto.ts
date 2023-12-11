b587158661c36562659d15321ac36bfe317b70ea
5tarry <hacherry05@naver.com>
2023-12-11 12:38:02 +0900

    feat : 회원가입/로그인 request dto 수정

--- server/src/auth/dto/signup-request.dto.ts
+++ server/src/auth/dto/signup-request.dto.ts
@@ -1,11 +1,23 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 import { UserDto } from 'src/user/dto/user.dto';
+
+enum PlatformEnum {
+  GOOGLE = 'GOOGLE',
+  KAKAO = 'KAKAO',
+}
 
 export class SignupRequestDto extends UserDto {
   /**
-   * 소셜 accessToken
-   * @example '1/fFAGRNJru1FTz70BzhT3Zg'
+   * 소셜 idToken
+   * @example '1234567890'
    */
   @IsNotEmpty()
-  accessToken: string;
+  idToken: string;
+
+  /**
+   * 소셜 platform 종류
+   * @example 'GOOGLE'
+   */
+  @IsEnum(PlatformEnum)
+  platform: PlatformEnum;
 }
